ffba1c891b66d0c1730d3af551aea84167256933
Rawda Yasser <rawdayasser2019@gmail.com>
2020-11-01 21:45:14 +0200

    Comments class : fixing typo in block comment & added javadoc comment

--- src/Comments.java
+++ src/Comments.java
@@ -1,8 +1,13 @@
 public class Comments {
     public static void main(String[] args) {
         // This is an inline comment
-        /**
+        /*
          * This is a block comment
+         */
+
+        /**
+         * This is added to javadoc :
+         *  to generate java software documentation
          */
     }
 }
